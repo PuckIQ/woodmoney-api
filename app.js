60b56b4512a823b2f47c7a490bd5c2d082039e7a
Zsolt Munoz <zmunoz@gmail.com>
2017-01-29 23:05:50 -0700

    Fixed problems with http & packages.json

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express'),
   app = express();
 
 var request = require('request'),
+  http = require('http'),
   routes = require('./routes'),
-  config = require('./conf.js');
+  config = require('./conf.js'),
+  server = http.createServer(app);
 
 routes(app, request);
 
