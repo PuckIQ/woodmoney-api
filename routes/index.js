4b43f67265e01f54df240050327b91f132ee7f31
Zsolt Munoz <zmunoz@gmail.com>
2017-07-25 12:48:48 -0600

    Added head to head WOWY comp

--- routes/index.js
+++ routes/index.js
@@ -43,7 +43,8 @@ module.exports = exports = function(app, request) {
     app.get('/wowy-player/:Player1Id', woodMoneyHandler.getSeasonWowy);
     app.get('/wowy-player/:Player1Id/season/:season', woodMoneyHandler.getSeasonWowy);
     app.get('/wowy-player/:Player1Id/season/:season/GameType/:gametype', woodMoneyHandler.getSeasonWowy);
-    
+    app.get('/wowy-player/:Player1Id/playercomp/:Player2Id', woodMoneyHandler.getSeasonWowy);
+
     app.get('/woodwowy-team/:Team', woodMoneyHandler.getSeasonWoodWowy);
     app.get('/woodwowy-team/:Team/season/:season', woodMoneyHandler.getSeasonWoodWowy);
     app.get('/woodwowy-team/:Team/season/:season/GameType/:gametype', woodMoneyHandler.getSeasonWoodWowy);
@@ -51,4 +52,5 @@ module.exports = exports = function(app, request) {
     app.get('/woodwowy-player/:Player1Id', woodMoneyHandler.getSeasonWoodWowy);
     app.get('/woodwowy-player/:Player1Id/season/:season', woodMoneyHandler.getSeasonWoodWowy);
     app.get('/woodwowy-player/:Player1Id/season/:season/GameType/:gametype', woodMoneyHandler.getSeasonWoodWowy);
+    app.get('/woodwowy-player/:Player1Id/playercomp/:Player2Id', woodMoneyHandler.getSeasonWowy);
 };
